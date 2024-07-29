9a3bfdf0dd37c62f93adc218b139da4bd86e015d
Friedel Schön <derfriedmundschoen@gmail.com>
2024-07-29 15:29:38 +0200

    make productiontool production-ready

--- pkgs/productiontool.nix
+++ pkgs/productiontool.nix
@@ -1,5 +1,15 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
+let
+  pyocdConfig = {
+    pack = [
+      (pkgs.fetchurl {
+        url = http://developer.nordicsemi.com/nRF5_SDK/pieces/nRF_DeviceFamilyPack/NordicSemiconductor.nRF_DeviceFamilyPack.8.44.1.pack;
+        hash = "sha256-lRNrV6gxC6NnqkbLZsjRSVYfx4dsXzc28yOp2/uPVZ4=";
+      })
+    ];
+  };
+in
 pkgs.appimageTools.wrapType2 rec {
   pname = "evajig-productiontool";
   version = "1.0.11";
@@ -11,8 +21,15 @@ pkgs.appimageTools.wrapType2 rec {
 
   extraPkgs = pkgs: with pkgs; [
     segger-jlink
-    python311
-    python311Packages.pyocd
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      pyocd
+    ]))
     dfu-util
   ];
+
+  profile = ''
+    export PYOCD_PROJECT_DIR=$(mktemp -d)
+
+    echo "${builtins.toJSON pyocdConfig}" > $PYOCD_PROJECT_DIR/pyocd.yaml
+  '';
 }
