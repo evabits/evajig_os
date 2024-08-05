c4b91dbad5d27489008067b19cc1424886c7dcf5
Friedel Schön <derfriedmundschoen@gmail.com>
2024-08-05 16:19:08 +0200

    add required pyocd-packs

--- pkgs/productiontool.nix
+++ pkgs/productiontool.nix
@@ -4,8 +4,16 @@ let
   pyocdConfig = {
     pack = [
       (pkgs.fetchurl {
-        url = http://developer.nordicsemi.com/nRF5_SDK/pieces/nRF_DeviceFamilyPack/NordicSemiconductor.nRF_DeviceFamilyPack.8.44.1.pack;
+        url = https://developer.nordicsemi.com/nRF5_SDK/pieces/nRF_DeviceFamilyPack/NordicSemiconductor.nRF_DeviceFamilyPack.8.44.1.pack;
         hash = "sha256-lRNrV6gxC6NnqkbLZsjRSVYfx4dsXzc28yOp2/uPVZ4=";
+      })
+      (pkgs.fetchurl {
+        url = "https://download.ambiq.com/packs/AmbiqMicro.Apollo_DFP.1.4.1.pack";
+        hash = "sha256-wIW4Ezh7QikJ0A3E8Wxbtbw8Vfw/KKHG9iYU3MOoBKU=";
+      })
+      (pkgs.fetchurl {
+        url = "https://packs.download.microchip.com/Microchip.SAMD21_DFP.3.6.144.pack";
+        hash = "sha256-l531IWnKPIxEzEbsz2x5l174IBjC0sS0vu1LSM3rVVM=";
       })
     ];
   };
